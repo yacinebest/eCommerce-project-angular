ced01f40aa575cb9e04f40c529085f4e0083edaf
yacinebest <yacine.besta@gmail.com>
2021-06-14 19:33:37 +0100

    10. eCommerce Project - Angular Front End - Product List

---
+++ src/app/common/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+
+
+
+    sku!: string;
+    name!: string;
+    description!: string;
+    unitPrice!: number;
+    imageUrl!: string;
+    active!: boolean;
+    unitsInStock!: string;
+    dateCreated!: Date;
+    lastUpdated!: Date;
+
+}
