0d96806a417c23e83fe0c871279697a969d11b9d
yacinebest <yacine.besta@gmail.com>
2021-06-15 09:36:16 +0100

    complete 12. eCommerce Project - Search for Products by Category

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,14 +12,30 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number = 0;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+
   }
   listProducts() {
-    this.productService.getProductList().subscribe(
+    //check if 'id' parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      //get the current category id and convert it ot number with +
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      //esle set current category id to 1
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
