f638df29915c0f6887f6bcbe6df3068f6880ce47
yacinebest <yacine.besta@gmail.com>
2021-06-17 19:17:58 +0100

    complete 25-26. Release 3.0 - Security - LoginLogout & User Registration --  But Not Working because Okta Login System Change from Tutorial

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa114wxz2KVnOMuS5d7',
+        issuer: 'https://dev-80749159.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
